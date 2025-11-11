c35183a6902d8a0018c0481da83fe28b31a4aac3
Pratik Patil <pratikpp555@gmail.com>
2025-11-11 21:50:43 +0530

    T1 : Project created

---
+++ src/main/java/com/textTranslator/Text_Translator/TextTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.textTranslator.Text_Translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/textTranslator/Text_Translator/TextTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.textTranslator.Text_Translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
