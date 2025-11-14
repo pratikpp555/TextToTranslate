0bd12f74647fed946bc1146d30fb6c35c2fea071
Pratik Patil <pratikpp555@gmail.com>
2025-11-15 00:00:47 +0530

    T3 : Added Null check

--- src/main/java/com/textTranslator/Text_Translator/service/TextTranslatorService.java
+++ src/main/java/com/textTranslator/Text_Translator/service/TextTranslatorService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 public class TextTranslatorService implements ITextTranslatorService{
     @Override
     public String getTranslatedText(String text) {
+        if(text.isEmpty() || text.isBlank()){
+            return "Text is Empty or null please send valid value";
+        }
         return null;
     }
 
